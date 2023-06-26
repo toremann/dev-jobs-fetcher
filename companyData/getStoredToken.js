3c21fe0b6654b91b029580b02cf90741d4da4336
toremann <50668920+toremann@users.noreply.github.com>
2023-06-26 09:12:18 +0200

    fix path

--- companyData/getStoredToken.js
+++ companyData/getStoredToken.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
+const path = require("path");
 
 async function readTokenFromFile() {
   try {
-    const tokenData = fs.readFileSync("token.json");
+    const filePath = path.resolve(__dirname, "token.json");
+    const tokenData = fs.readFileSync(filePath);
     const token = JSON.parse(tokenData).token;
-    console.log('stored token,', token)
+    console.log('stored token:', token);
     return token;
   } catch (error) {
     console.error("Error occurred while reading token from file:", error);
